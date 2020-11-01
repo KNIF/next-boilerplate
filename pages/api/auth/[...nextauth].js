07da8509b63d68cc25570ce4eea2338b6bdc4d32
KNIF <admin@knif.dev>
2020-11-01 15:48:09 +0100

    Added Auth0 Provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,13 +3,18 @@ import Providers from 'next-auth/providers';
 
 const options = {
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Providers.Auth0({
+      clientId: process.env.AUTH0_ID,
+      clientSecret: process.env.AUTH0_SECRET,
+      domain: process.env.AUTH0_DOMAIN,
     }),
     Providers.Discord({
       clientId: process.env.DISCORD_ID,
       clientSecret: process.env.DISCORD_SECRET,
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   database: process.env.DATABASE_URL,
