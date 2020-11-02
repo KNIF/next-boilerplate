1f1cb3a7883e89af8e672d08dc2b480255561952
KNIF <admin@knif.dev>
2020-11-02 14:54:00 +0100

    Added basic Fauna API Function

---
+++ pages/api/fauna/collections.js
@@ -0,0 +1,32 @@
+import faunadb, { query as q } from 'faunadb';
+
+const { FAUNADB_SECRET: secret } = process.env;
+
+let client;
+
+if (secret) {
+  client = new faunadb.Client({ secret });
+}
+
+export default async (req, res) => {
+  try {
+    let collections = [];
+
+    if (!client) {
+      return res
+        .status(500)
+        .json({ error: new Error('Missing secret to connect to FaunaDB') });
+    }
+
+    await client
+      .paginate(q.Collections())
+      .map((ref) => q.Get(ref))
+      .each((page) => {
+        collections = collections.concat(page);
+      });
+
+    res.json({ collections });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
